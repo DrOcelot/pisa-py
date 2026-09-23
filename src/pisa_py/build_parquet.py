41eb04ff5a423b87b95ca09ebc15b10778e6b1f7
DrOcelot <owenpauljames@gmail.com>
2026-09-23 10:51:21 +0100

    buildparquet with missing values pulled out

--- src/pisa_py/build_parquet.py
+++ src/pisa_py/build_parquet.py
@@ -7,43 +7,35 @@ import numpy as np
 import pandas as pd
 import pyreadstat
 
-sch_path = Path(__file__).resolve().parents[2] / "data" / "spss" / "CY08MSP_SCH_QQQ.SAV"
-stu_path = Path(__file__).resolve().parents[2] / "data" / "spss" / "CY08MSP_STU_QQQ.SAV"
+sch_22_path = Path(__file__).resolve().parents[2] / "data" / "spss" / "CY08MSP_SCH_QQQ.SAV"
+stu_22_path = Path(__file__).resolve().parents[2] / "data" / "spss" / "CY08MSP_STU_QQQ.SAV"
+out_path = Path(__file__).resolve().parents[2] / "data" / "built"
 
-data, meta = pyreadstat.read_sav(sch_path, metadataonly=False, user_missing=True)
-values = pd.DataFrame(meta.variable_value_labels)
-meta_missing_ranges = meta.missing_ranges["SC014Q01TA"]
-org = values[values["SC014Q01TA"].notna()]
-#print(org["SC014Q01TA"])
-#print(meta_missing_ranges[0]["lo"]) 
+# This line chooses which dataset to use.
+sav = pyreadstat.read_sav(sch_22_path, metadataonly=False, user_missing=True)
 
-def add_missing_reasons(path: Path, var: str) -> pd.DataFrame:
+def add_missing_reasons(df: pd.DataFrame, var: str) -> pd.DataFrame:
     """Add missing reasons to the school survey data."""
     # grab data and metadata from whole spss file
-    data, meta = pyreadstat.read_sav(path, metadataonly=False, user_missing=True) 
+    data, meta = df
 
-    #gets key value pairs for "lo" and "hi" indicating the range of the data's missing range
+    # gets key value pairs for "lo" and "hi" indicating the range of the data's missing range
     meta_missing_ranges = meta.missing_ranges[var] 
 
-    #extracts the missing low and high values
+    # extracts the lowest value that indicates missingness
     lo = meta_missing_ranges[0]["lo"]
-    hi = meta_missing_ranges[0]["hi"]
 
-    #makes a dictionary of all labels the variable has
-    #if the var is numeric labels will be NaN for real values
-    #if the var is categoric labels will not be NaN, instead they will match the categoric answer
-    labels_dict = meta.variable_value_labels[var]
-    print(labels_dict)
-    print(lo)
-    print(data[data[var] >= lo][var])
-    
-        
-
-    #missing_labels =  labels_dict.filter(value >= lo)
-    #print(missing_labels)
-
-    #add an NaN column with missing reason to be populated later.
+    # add an NaN column with missing reason to be populated later.
     data[f"{var}_missing_reason"] = np.nan
 
-#add_missing_reasons(sch_path, "SC014Q01TA")
-add_missing_reasons(stu_path, "ST261Q03JA")
+    # makes a dictionary of all labels the variable has
+    # if the var is numeric labels will be NaN for real values
+    # if the var is categoric labels will not be NaN, instead they will match the categoric answer
+    labels_dict = meta.variable_value_labels[var]
+    data[f"{var}_missing_reason"] = np.where(data[var]<lo, np.nan, data[var].map(labels_dict))
+    data[var] = np.where(data[var]>=lo, np.nan, data[var].map(labels_dict).fillna(data[var]))
+    return data
+
+pimp = add_missing_reasons(sav, "SC016Q02TA")
+print(pimp["SC016Q02TA"])
+#add_missing_reasons(stu_22_path, "ST261Q03JA")
